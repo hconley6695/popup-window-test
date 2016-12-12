201aaeaf1d0da7345a052980a90073299e903587
Heather Conley <hconley6695@gmail.com>
2016-12-12 16:52:37 -0500

    linking javascript to html

---
+++ index.js
@@ -0,0 +1,24 @@
+
+import $ from 'jquery.js';
+// function popitup(url) {
+// 	newwindow=window.open(url,'name','height=200,width=150');
+// 	if (window.focus) {
+//     newwindow.focus()
+//   }
+// 	return false;
+// }
+
+// function popitup2() {
+// 	newwindow2=window.open('','name','height=200,width=150');
+// 	var tmp = newwindow2.document;
+// 	tmp.write('<html><head><title>popup</title>');
+// 	tmp.write('<link rel="stylesheet" href="js.css">');
+// 	tmp.write('</head><body><p>Are you sure you want to go to another site?</p>');
+// 	tmp.write('<p><button onClick="javascript:self.close()"><a href="http://www.tinyurl.com/heatherconley" target="_blank">Yes</a></button>.</p>');
+// 	tmp.write('<p><a href="javascript:self.close()">No</a> </p>');
+// 	tmp.write('</body></html>');
+// 	tmp.close();
+// }
+
+
+console.log("Hello there!");
