e6c7d4da7ab9ef86c96a9c4a52e52f5b1a14883d
Heather Conley <hconley6695@gmail.com>
2016-12-12 21:36:54 -0500

    pressing button brings up a pop-up window, asking you if you want to continue to new site.  if press yes, new site opens in new tab.  if press no, pop up window closes and you return to same site

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 
-import $ from 'jquery.js';
-// function popitup(url) {
-// 	newwindow=window.open(url,'name','height=200,width=150');
-// 	if (window.focus) {
-//     newwindow.focus()
-//   }
-// 	return false;
-// }
+// import $ from 'jquery.js';
 
-// function popitup2() {
-// 	newwindow2=window.open('','name','height=200,width=150');
-// 	var tmp = newwindow2.document;
-// 	tmp.write('<html><head><title>popup</title>');
-// 	tmp.write('<link rel="stylesheet" href="js.css">');
-// 	tmp.write('</head><body><p>Are you sure you want to go to another site?</p>');
-// 	tmp.write('<p><button onClick="javascript:self.close()"><a href="http://www.tinyurl.com/heatherconley" target="_blank">Yes</a></button>.</p>');
-// 	tmp.write('<p><a href="javascript:self.close()">No</a> </p>');
-// 	tmp.write('</body></html>');
-// 	tmp.close();
-// }
+
+
+function popitup() {
+	newwindow=window.open('','name','height=200,width=150');
+	var tempWindow = newwindow.document;
+	tempWindow.write('<html><head><title>popup</title>');
+	tempWindow.write('<link rel="stylesheet" href="js.css">');
+	tempWindow.write('</head><body><p>Are you sure you want to go to another site?</p>');
+	tempWindow.write('<p><button onClick="javascript:self.close()"><a href="http://www.tinyurl.com/heatherconley" target="_blank">Yes</a></button>.</p>');
+	tempWindow.write('<p><a href="javascript:self.close()">No</a> </p>');
+	tempWindow.write('</body></html>');
+	tempWindow.close();
+}
 
 
 console.log("Hello there!");
